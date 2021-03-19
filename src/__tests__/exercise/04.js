9e86e57f1e686f8182de9e7a0cc8a149ca68cfc4
Matthew Bender <matthew-bender@cirrusmd.com>
2021-03-19 17:08:31 -0600

    exercise 4 - extra credit 2 & 3

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,14 +5,23 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
+
+function buildLoginForm(overrides) {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...overrides,
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
 
   render(<Login onSubmit={handleSubmit} />)
 
-  const username = 'codebender'
-  const password = 'secret password'
+  const {username, password} = buildLoginForm()
+
   const usernameField = screen.getByLabelText(/username/i)
   const passwordField = screen.getByLabelText(/password/i)
   const submitButton = screen.getByRole('button', {name: /submit/i})
