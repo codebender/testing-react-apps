ed519a3f9409fc82ac16c92a627c1eb1bd5a8600
Matthew Bender <matthew-bender@cirrusmd.com>
2021-04-02 14:03:43 -0600

    exercise 8 - extra credit 3

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,23 +1,11 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import * as React from 'react'
-import {render, act} from '@testing-library/react'
+import {renderHook, act} from '@testing-library/react-hooks'
 import useCounter from '../../components/use-counter'
 
-function setup({initProps} = {}) {
-  const result = {}
-  function TestComponent(props) {
-    result.current = useCounter(props)
-    return null
-  }
-
-  render(<TestComponent {...initProps} />)
-  return result
-}
-
 test('exposes the count and increment/decrement functions', () => {
-  const result = setup()
+  const {result} = renderHook(useCounter)
 
   expect(result.current.count).toBe(0)
   act(() => result.current.increment())
@@ -27,7 +15,7 @@ test('exposes the count and increment/decrement functions', () => {
 })
 
 test('allows customization of the initial count', () => {
-  const result = setup({initProps: {initialCount: 5}})
+  const {result} = renderHook(useCounter, {initialProps: {initialCount: 5}})
 
   expect(result.current.count).toBe(5)
   act(() => result.current.increment())
@@ -37,7 +25,7 @@ test('allows customization of the initial count', () => {
 })
 
 test('allows customization of the step', () => {
-  const result = setup({initProps: {step: 5}})
+  const {result} = renderHook(useCounter, {initialProps: {step: 5}})
 
   expect(result.current.count).toBe(0)
   act(() => result.current.increment())
@@ -47,7 +35,9 @@ test('allows customization of the step', () => {
 })
 
 test('allows customization of the step and initial count', () => {
-  const result = setup({initProps: {initialCount: 5, step: 5}})
+  const {result} = renderHook(useCounter, {
+    initialProps: {initialCount: 5, step: 5},
+  })
 
   expect(result.current.count).toBe(5)
   act(() => result.current.increment())
